fa67a05f80282be638b46d3e741241adfb981fd0
Zhou Hao <zhouhao@nacooh.com>
2015-08-26 21:07:13 +0800

    Initial Commit

---
+++ AutoScrollview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoScrollview
+//
+//  Created by Zhou Hao on 26/08/15.
+//  Copyright © 2015年 Zeus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
